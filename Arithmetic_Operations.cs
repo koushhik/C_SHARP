86869ecde06775c20dae2b792a3f33c8de02789b
Koushik Chinnappa T.K <54579643+koushhik@users.noreply.github.com>
2023-11-02 17:16:38 +0530

    4. Write a C# Sharp program to print the results of the specified operations.           Test data:           -1 + 4 * 6          ( 35+ 5 ) % 7          14 + -4 * 6 / 11          2 + 15 / 6 * 1 - 7 % 2
    4. Write a C# Sharp program to print the results of the specified operations.
     
    Test data:
             -1 + 4 * 6
             ( 35+ 5 ) % 7
             14 + -4 * 6 / 11
             2 + 15 / 6 * 1 - 7 % 2

---
+++ Arithmetic_Operations.cs
@@ -0,0 +1,27 @@
+/*
+          4. Write a C# Sharp program to print the results of the specified operations.
+          Test data:
+
+         -1 + 4 * 6
+         ( 35+ 5 ) % 7
+         14 + -4 * 6 / 11
+         2 + 15 / 6 * 1 - 7 % 2
+
+*/
+
+using System;
+
+namespace Practice
+{
+    class program   
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(-1 + 4 * 6);   //FOR THE GIVEN TEST CASES ENTER THE OPERATIONS , HERE THE COMPILOR IS MART ENOUGH TO CHECK WHICH-
+            Console.WriteLine((35 + 5) % 7);         //cont...RULE NEEDS TO BE CHECKED FIRST.
+            Console.WriteLine(14 + -4 * 6 / 11);
+            Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+        }
+    }
+
+}
