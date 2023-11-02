4e4a5fd4db4d9d3e1c5c23c7405881df8be47508
Koushik Chinnappa T.K <54579643+koushhik@users.noreply.github.com>
2023-11-02 18:05:12 +0530

    6) Write a C# Sharp program to print the output of the multiplication of three numbers entered by the user.
    6) Write a C# Sharp program to print the output of the multiplication of three numbers entered by the user.
    
    we take input from users using ReadLine() for a , b and c
    
    then we use * for multiplication 
    
    making it a*b*c . then printing it.

---
+++ 6) multiplication_3_numbers.cs
@@ -0,0 +1,21 @@
+/*
+         6) Write a C# Sharp program to print the output of the multiplication of three numbers entered by the user.
+
+*/
+
+using System;
+
+namespace Practice
+{
+    class program   
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine()); //WE TAKE INPUTS FROM THE USERS FOR 3 VALUES OF INTEGER TYPE
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a * b * c); //WE USE * OPERATOR FOR MULTIPLICATION OF 3 VALUES a ,b AND c THUS a*b*b and display the result
+        }
+    }
+
+}
