53143908db8fadf1ce4eedd141ff76eb46aad44b
Koushik Chinnappa T.K <54579643+koushhik@users.noreply.github.com>
2023-11-02 18:54:37 +0530

    10. Write a C# Sharp program that takes three numbers (x,y,z) as input and outputs (x+y).z and x.y + y.z
    10. Write a C# Sharp program that takes three numbers (x,y,z) as input and outputs (x+y).z and x.y + y.z
    
    WE TEKE 3 NUMBERS X , Y AND Z AS INPUT FROM THE USERS
    WE THEN PERFORM ARITHEMATIC OPERATIONS LIKE (X + Y)*Z
    AND (X * Y) + (Y * Z)
    
    THUS THE OUTPUT WILL LOOK LIKE:
    2
    1
    2
    
    ANSWER:
    6
    4
    
    
    
    EXPLAINATION : 
     
    FOR (X +Y)*Z
        (2 + 1) * 2 
         3*2 = 6
    
    FOR (X*Y)+(Y*Z)
         (2*1) + (1*2)
          2 + 2 = 4

---
+++ 10) perform_(x+y).z_and_(x.y+y.z).cs
@@ -0,0 +1,23 @@
+
+/*
+        10. Write a C# Sharp program that takes three numbers (x,y,z) as input and outputs (x+y).z and x.y + y.z
+
+*/
+
+using System;
+
+namespace Practice
+{
+    class program   
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());   //WE TEKE 3 NUMBERS X , Y AND Z AS INPUT FROM THE USERS
+            int y = Convert.ToInt32(Console.ReadLine());
+            int z = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine((x + y)*z);                   //WE THEN PERFORM ARITHEMATIC OPERATIONS LIKE (X + Y)*Z
+            Console.WriteLine((x*y) + (y * z));             //AND (X * Y) + (Y * Z)
+        }
+    }
+
+}
