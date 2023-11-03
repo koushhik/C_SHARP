481a0181263def118dcd5b578ac1bbf843ed9ec7
Koushik Chinnappa T.K <54579643+koushhik@users.noreply.github.com>
2023-11-03 10:19:30 +0530

    11. Write a C# Sharp program that takes an age (for example 20) as input and prints something like "You look older than 20".
    11. Write a C# Sharp program that takes an age (for example 20) as input and prints something like "You look older than 20".
    
    
    HERE THE MAIN GOAL IS TO TAKE INPUT FROM THE USER THROUGH READLINE AND STORE IT IN AN INTEGER VARIABLE. 
    
    THEN TO DIAPLAY THE MENTION LINE ...THE KEY IS TO UNDERSTAND BASIC INPUT OUTPUT OPERATIONS AND SPACING.
    
    
    so to display the english line:
    Example:
    you look older than 40  
    
    inside Console.WriteLine() -> we use :
    ("you look older than " + a)
      you look older than 40   -if user enter a as 40.
    
    here,
     a is the integer input which user gives 
     after the line "...than "  we give one space to make it seemless.

---
+++ 11) older_than_age.cs
@@ -0,0 +1,20 @@
+
+/*
+       11. Write a C# Sharp program that takes an age (for example 20) as input and prints something like "You look older than 20".
+
+*/
+
+using System;
+
+namespace Practice
+{
+    class program   
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());       //HERE THE MAIN GOAL IS TO TAKE INPUR FROM THE USER THROUGH READLINE AND STORE IT IN AN INTEGER VARIABLE. 
+            Console.WriteLine("You look older than " + a);     //THEN TO DIAPLAY THE MENTION LINE ...THE KEY IS TO UNDERSTAND BASIC INPUT OUTPUT OPERATIONS AND SPACING.
+        }
+    }
+
+}
