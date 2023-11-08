7d22a781d56daeed544194abfac6c48c603355b2
Koushik Chinnappa T.K <54579643+koushhik@users.noreply.github.com>
2023-11-08 15:21:04 +0530

    27.  Write a C# program to create a string where the first 4 characters are in lower case. If the string is less than 4 letters, make the whole string in uppercase.
    27.  Write a C# program to create a string where the first 4 characters are in lower case. If the string is less than 4 letters, make the whole string in uppercase.
    
    -> we read the string using ReadLine()
    ->we check if string length is less than 4 . if(str.Length < 4) : if string length is less than 4 we convert it to UPPER CASE
    ->str.ToUpper() :string function to convert string to upper case.
    ->If string is greater than 4 we create a range using substring : str.Substring(0,4).ToLower().
    ->That is first 4 char in string will be in lower case while the remaining string from (4 to string length - 4) would be as it is : str.Substring(4, str.Length -4)

---
+++ 27)alternate_upper_to_lowercase_within_specified_char_range.cs
@@ -0,0 +1,41 @@
+
+/*    
+             27.  Write a C# program to create a string where the first 4 characters are in lower case. If the string is less than 4 letters, make the whole string in upper case.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Net;
+using System.Runtime.Remoting.Services;
+using System.Xml.Schema;
+
+namespace Practice
+{
+    class Program
+    {
+        public void charr()
+        {
+          Console.WriteLine("ENTER YOUR STRING :");
+            string str = Console.ReadLine();              //reading the string
+            if(str.Length <= 4)                           //if string length is less than 4 we convert it to UPPER CASE
+            {
+                Console.WriteLine(str.ToUpper());           //string function to convert string to upper case.
+            }
+            else
+            {
+                Console.WriteLine(str.Substring(0,4).ToLower() + str.Substring(4, str.Length -4)); //If string is greater than 4 we create a reange uising substring
+                                               //That is first 4 char in string will be in lower case while the reamining string from (4 to string length - 4) would be as it is. 
+            }
+        }
+
+        public static  void Main(string[] args)
+        {
+            Program lw = new Program();
+            lw.charr();
+        }
+
+    }
+}
+
