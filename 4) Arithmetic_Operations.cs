b9091a5418a613330a742bd770f0c2a2d98e5489
Koushik Chinnappa T.K <54579643+koushhik@users.noreply.github.com>
2023-11-14 22:03:37 +0530

    Update 4) Arithmetic_Operations.cs

--- 4) Arithmetic_Operations.cs
+++ 4) Arithmetic_Operations.cs
@@ -18,7 +18,7 @@ namespace Practice
         static void Main(string[] args)
         {
             Console.WriteLine(-1 + 4 * 6);   //FOR THE GIVEN TEST CASES ENTER THE OPERATIONS , HERE THE COMPILOR IS MART ENOUGH TO CHECK WHICH-
-            Console.WriteLine((35 + 5) % 7);         //cont...RULE NEEDS TO BE CHECKED FIRST.
+            Console.WriteLine((35 + 5) % 7);         //cont......RULE NEEDS TO BE CHECKED FIRST.
             Console.WriteLine(14 + -4 * 6 / 11);
             Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
         }
