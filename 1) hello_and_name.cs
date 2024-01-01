4124d165aa3e86e641d45b27e87f52d12986be88
Koushik Chinnappa T.K <54579643+koushhik@users.noreply.github.com>
2024-01-01 21:22:39 +0530

    Update 1) hello_and_name.cs

--- 1) hello_and_name.cs
+++ 1) hello_and_name.cs
@@ -8,7 +8,7 @@ using System;
 using System.Collections.Generic;   
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks; //No need to use all the headerfiles 
 
 namespace Practice
 {
@@ -17,7 +17,7 @@ namespace Practice
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");  //HERE WE PRINT THE NAME DIRECTLEY BY GIVING INPUT TO THROUGH READ-LINE STATEMENT
-            Console.WriteLine("KOUSHIK CHIN");  //WE COULD USE Console.Write() BUT IT WILL PRINT IN STRAIGHT LINE SO WE USE Console.WriteLine() FOR NEW LINE.
+            Console.WriteLine("KOUSHIK CHINNAPPA");  //WE COULD USE Console.Write() BUT IT WILL PRINT IN STRAIGHT LINE SO WE USE Console.WriteLine() FOR NEW LINE.
 
             //IT IS ALWAYS A GOOD PRACTICE TO TAKE INPUT FROM THE USER WITH STRING DATA TYPE AND ADD Console.ReadLine() STATEMENT.
             //EXAMPLE: string x = Console.ReadLine();
